a4ebe7e5143775431b4a16ca7f8ee05e22a115a4
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-03-04 10:51:31 +0600

    update alert and use confirm

--- alert-confirm-prompt.js
+++ alert-confirm-prompt.js
@@ -1,3 +1,9 @@
-const alertFunc = () => {
-    alert("This is an alert!!!");
+const alertFunc = (getData) => {
+    alert(getData);
+};
+
+const confirmFunc = () => {
+    // const isTrue = confirm("Are you a programmer?");
+    // isTrue ? alertFunc("Mashallah!") : alertFunc("It's ok no problem.");
+    confirm ("Are you a programmer?") ? alertFunc("Mashallah!") : alertFunc("It's ok no problem.");
 };
