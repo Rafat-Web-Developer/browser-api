ece7043a74d69af199efae4b41ceffe6e928afef
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-03-04 11:22:11 +0600

    Update location

--- location.js
+++ location.js
@@ -1,15 +1,17 @@
 // const location = window.location;
 const getLocation = location;
 
-console.log(getLocation);
-console.log(`Hash = ${getLocation.hash || null}`);
-console.log(`Host = ${getLocation.host || null}`);
-console.log(`Host name = ${getLocation.hostname || null}`);
-console.log(`Href = ${getLocation.href || null}`);
-console.log(`Origin = ${getLocation.origin || null}`);
-console.log(`Path name = ${getLocation.pathname || null}`);
-console.log(`Port = ${getLocation.port || null}`);
-console.log(`Protocol = ${getLocation.protocol || null}`);
+const getLocationBtn = () => {
+    console.log(getLocation);
+    console.log(`Hash = ${getLocation.hash || null}`);
+    console.log(`Host = ${getLocation.host || null}`);
+    console.log(`Host name = ${getLocation.hostname || null}`);
+    console.log(`Href = ${getLocation.href || null}`);
+    console.log(`Origin = ${getLocation.origin || null}`);
+    console.log(`Path name = ${getLocation.pathname || null}`);
+    console.log(`Port = ${getLocation.port || null}`);
+    console.log(`Protocol = ${getLocation.protocol || null}`);
+};
 
 const reloadFunc = () => {
     getLocation.reload();
